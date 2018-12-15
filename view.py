68a130dc3cd73fd0efaf440d88067850a09fcb24
SwagXM <215120986@qq.com>
2018-12-15 16:44:54 +0800

    添加源文件

---
+++ view.py
@@ -0,0 +1,23 @@
+# from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def count(request):
+    user_text = request.GET['text']
+    total_count = len(user_text)
+    word_dict = {}
+    for word in user_text:
+        if word not in word_dict:
+            word_dict[word] = 1
+        else:
+            word_dict[word] += 1
+
+    sort_dict = sorted(word_dict.items(), key=lambda w: w[1], reverse=True)
+
+    return render(request, 'count.html',
+                  {'count': total_count, 'text': user_text,
+                   'worddict': word_dict, 'sorted': sort_dict})
